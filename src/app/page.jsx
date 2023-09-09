9e9573c7d6c3bdb0a20883b81c3ae4261db09d1c
enemuo <francenemuo@gmail.com>
2023-09-09 12:54:32 +0100

    feat: create routes on home page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,16 +1,49 @@
+import Link from 'next/link'
+import styles from './styles/home.module.css'
+
 export default function Home() {
   return (
     <main>
-      <ul>
+      <ul className={styles.homeUl}>
+
+        <Link href='/'>
+        <li>Home</li>
+        </Link>
+        <Link href='/CreateAccount'>
         <li>create account</li>
+        </Link>
+
+        <Link href='/ForgotPassword'>
         <li>forgot password</li>
+        </Link>
+
+        <Link href='/ResetPassword'>
         <li>reset password</li>
+        </Link>
+
+        <Link href='/SuccessAccountCreated'>
         <li>account Success</li>
+        </Link>
+
+        <Link href='/SuccessPasswordReset'>
         <li>password  success</li>
+        </Link>
+
+        <Link href='/Verification'>
         <li>verification</li>
+        </Link>
+
+        <Link href='/Deposit'>
         <li>deposit</li>
+        </Link>
+
+        <Link href='/Signup'>
         <li>sign up</li>
+        </Link>
+
+        <Link href='/Login'>
         <li>log in</li>
+        </Link>
       </ul>
     </main>
   )
