810f335c54a6bbaf23094cab14c6b46b1e1ed378
enemuo <francenemuo@gmail.com>
2023-10-05 09:57:47 +0100

    fix: change to image component for the logo

--- src/app/components/Logo.jsx
+++ src/app/components/Logo.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import Image from 'next/image'
 
 export default function Logo(props) {
   return (
     <span className='h-[auto] w-[auto] flex items-center justify-center'>
-        <img src='Logo lockup Black H 2.svg' width={ props.width ? props.width : 100 }/>
+        <Image src='/Logo lockup Black H 2.svg' alt='airniro logo' height={100} width={ props.width ? props.width : 100 } className='inline'/>
     </span>
   )
 }
