8f5484d4cc684b66f2b5428be04ff1f5d53f7fd6
enemuo <francenemuo@gmail.com>
2023-10-05 09:30:33 +0100

    fix: create a nav component

---
+++ src/app/NavCom.jsx
@@ -0,0 +1,48 @@
+import Link from 'next/link'
+import styles from './styles/home.module.css'
+
+export default function NavCom() {
+  return (
+    <ul className={styles.homeUl}>
+
+        <Link href='/HomePage'>
+        <li>Home</li>
+        </Link>
+        <Link href='/CreateAccount'>
+        <li>create account</li>
+        </Link>
+
+        <Link href='/ForgotPassword'>
+        <li>forgot password</li>
+        </Link>
+
+        <Link href='/ResetPassword'>
+        <li>reset password</li>
+        </Link>
+
+        <Link href='/SuccessAccountCreated'>
+        <li>account Success</li>
+        </Link>
+
+        <Link href='/SuccessPasswordReset'>
+        <li>password  success</li>
+        </Link>
+
+        <Link href='/Verification'>
+        <li>verification</li>
+        </Link>
+
+        <Link href='/Deposit'>
+        <li>deposit</li>
+        </Link>
+
+        <Link href='/Register'>
+        <li>sign up</li>
+        </Link>
+
+        <Link href='/Register'>
+        <li>log in</li>
+        </Link>
+      </ul>
+  )
+}
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,50 +1,11 @@
-import Link from 'next/link'
-import styles from './styles/home.module.css'
+
+import HomePage from './HomePage/page'
 
 export default function Home() {
   return (
     <main>
-      <ul className={styles.homeUl}>
-
-        <Link href='/HomePage'>
-        <li>Home</li>
-        </Link>
-        <Link href='/CreateAccount'>
-        <li>create account</li>
-        </Link>
-
-        <Link href='/ForgotPassword'>
-        <li>forgot password</li>
-        </Link>
-
-        <Link href='/ResetPassword'>
-        <li>reset password</li>
-        </Link>
-
-        <Link href='/SuccessAccountCreated'>
-        <li>account Success</li>
-        </Link>
-
-        <Link href='/SuccessPasswordReset'>
-        <li>password  success</li>
-        </Link>
-
-        <Link href='/Verification'>
-        <li>verification</li>
-        </Link>
-
-        <Link href='/Deposit'>
-        <li>deposit</li>
-        </Link>
-
-        <Link href='/Register'>
-        <li>sign up</li>
-        </Link>
-
-        <Link href='/Register'>
-        <li>log in</li>
-        </Link>
-      </ul>
+      <HomePage />
+      
     </main>
   )
 }
