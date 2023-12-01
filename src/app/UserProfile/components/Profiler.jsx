5f6d505876920a2a69c3ef7d0e8c87e85033a4f2
enemuo <francenemuo@gmail.com>
2023-12-01 18:50:16 +0100

    fix: provide index as key props for Profiler component

--- src/app/UserProfile/components/Profiler.jsx
+++ src/app/UserProfile/components/Profiler.jsx
@@ -16,14 +16,14 @@ export default function Profiler(props) {
             
             {
                 isEdit?
-                props.detail.map((item)=>(
-                    <label className='min-w-[30%] w-[auto] my-3'>
+                props.detail.map((item, index)=>(
+                    <label className='min-w-[30%] w-[auto] my-3' key={index}>
                         <p id='input-group__input' className='text-[14px] text-[#2b2b2bdb] '>{item.name}</p>
                         <input type={item.type? item.type: "text"} id='input-group__label' className='text-[16px] border-[1px] rounded-md outline-none h-[auto] w-[auto]' placeholder={`${item.value.length > 25 ? item. value.slice(0,25) + `...`: item.value}`} onChange={item.onChange} ref={item.ref}/>
                     </label>
                 )):
-                props.detail.map((item)=>(
-                    <div className='min-w-[30%] w-[auto] my-3'>
+                props.detail.map((item, index)=>(
+                    <div className='min-w-[30%] w-[auto] my-3' key={index}>
                         <p className='text-[14px] text-[#2b2b2bdb]'>{item.name}</p>
                         <p className='text-[16px]  text-black'>{item.value.length > 25 ? item.value.slice(0,25) + `...`: item.value}</p>
                     </div>
