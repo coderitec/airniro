a4ef2f6bd8238cb8f3285345c98723316b1741ae
enemuo <francenemuo@gmail.com>
2023-12-02 14:58:57 +0100

    fix: change company logo

--- src/app/components/Logo.jsx
+++ src/app/components/Logo.jsx
@@ -4,7 +4,7 @@ import Image from 'next/image'
 export default function Logo(props) {
   return (
     <span className='h-[auto] w-[auto] flex items-center justify-center'>
-        <Image src='/Logo lockup Black H 2.svg' alt='airniro logo' height={100} width={ props.width ? props.width : 100 } className='inline'/>
+        <Image src='/Logo Horizontal lockup white 2.svg' alt='airniro logo' height={100} width={ props.width ? props.width : 100 } className='inline'/>
     </span>
   )
 }
